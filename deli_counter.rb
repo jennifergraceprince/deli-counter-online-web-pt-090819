5f7c076ed1344cc732fb84f5db49b0c428d672f6
Jennifer Prince <jennifergraceprince@gmail.com>
2019-10-07 04:54:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,12 @@ end
 
 line(katz_deli)
 
-def now_serving
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
 end
 
 now_serving(katz_deli)
