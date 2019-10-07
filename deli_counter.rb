25c7bc8d5bf0ed866323522f1ed93fee4f059b5a
Jennifer Prince <jennifergraceprince@gmail.com>
2019-10-07 04:55:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,6 @@ def line(array)
     puts "The line is currently empty."
   else 
     message = "The line is currently:"
-  end
   
   array.each_with_index do |value, index| 
     message += " #{index.to_i+1}. #{value}"
